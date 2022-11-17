776ad160254dc3fac2660121d9f2092e21d13cd3
Daanyaal Tahir <tahir.daanyaal@gmail.com>
2022-11-17 15:33:12 -0500

    Add changes to dashbaord

--- src/client/src/pages/HomePage/index.js
+++ src/client/src/pages/HomePage/index.js
@@ -1,9 +1,41 @@
-import { Layout } from "antd";
 import React, { useState } from "react";
-const { Content, Sider } = Layout;
+import { Col, Divider, Row, Card } from "antd";
 
 const HomePage = () => {
-  return <div className="gf_general_page">this the homepage</div>;
+  return (
+    <Col>
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </Col>
+        <Col span={24}>
+          <Card title="Card title" bordered={false}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </Col>
+      </Row>
+    </Col>
+  );
 };
 
 export default HomePage;
